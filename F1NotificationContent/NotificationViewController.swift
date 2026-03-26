3e268fc199cde674084324ce1567974cb92ecd6c
Adam Samuel Štefánik <adamm.stefanik11@gmail.com>
2026-03-26 11:37:29 +0100

    Remove duplicate track assets from widget extension

---
+++ F1NotificationContent/NotificationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NotificationViewController.swift
+//  F1NotificationContent
+//
+//  Created by Adam S. Štefánik on 26/03/2026.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
